bde2863a75c29042b9c2c00b37dbaf7d3586cf00
Raghav <raghav.grover2121@gmail.com>
2019-11-01 01:22:45 +0530

    added commets and added delete db call for admin functionality

--- services/DAO/adminDAO.js
+++ services/DAO/adminDAO.js
@@ -1,7 +1,8 @@
 const models = require("../../models/index");
 
+//ALL DATABASE CALL FUNCTIONS ARE DEFINED HERE RELATED TO ADMIN
 class adminDAO{
-    
+    //DB CALL TO GET ALL PRODUCT
     getAllProducts(){
      return new Promise((resolve,reject) =>{
         models.Product.findAll().then(products =>{
@@ -15,14 +16,28 @@ class adminDAO{
         })
      })
     }
-
-
+    
+    ///DB CALL TO ADD PRODUCT
     addProduct(product){
         return new Promise((resolve,reject)=>{
             models.Product.create(product).then(()=>{
                 console.log("product added")
                 resolve("added into the db");
             }).catch(err =>{
+                console.log(err);
+                reject(err);
+            })
+        })
+    }
+
+    //DB CALL TO DELETE PRODUCT
+    deleteProduct(productId) {
+        return new Promise( (resolve,reject) => {
+            models.Product.destroy({
+                where : { id : productId }
+            }).then( () => {
+                resolve("successfully deleted product");
+            }).catch( (err)=>{
                 console.log(err);
                 reject(err);
             })

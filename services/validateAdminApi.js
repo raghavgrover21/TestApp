3709ae80395d5e8ab26e43f4db622d6911758b54
Raghav <raghav.grover2121@gmail.com>
2019-11-01 01:38:58 +0530

    validations for the data entered by user or admin

---
+++ services/validateAdminApi.js
@@ -0,0 +1,4 @@
+//middleware to valid all the variables which the user will be sending
+//check constrains for name === string and id === number
+//also check if the id of a product entered by user is valid or not
+//return a error message here itself no db call can be made (saves a db call and time)
